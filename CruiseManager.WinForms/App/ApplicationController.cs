3de3a3d92e213bf343ad608651cac0d33e557a51
Ben Campbell <benjaminjcampbell@fs.fed.us>
2017-01-25 17:26:31 -0700

    fix cruiseDAL references, move Main()

--- CruiseManager.WinForms/App/ApplicationController.cs
+++ CruiseManager.WinForms/App/ApplicationController.cs
@@ -22,19 +22,6 @@ namespace CruiseManager.WinForms.App
             this.WindowPresenter.ShowHomeLayout();
         }
 
-        //public ApplicationControllerWinForms() : this(WindowPresenter.Instance, UserSettings.Instance, SetupService.Instance) { }
-
-        //public ApplicationControllerWinForms(
-        //    WindowPresenter windowPresenter,
-        //    ExceptionHandler exceptionHandler,
-        //    IUserSettings userSettings,
-        //    SetupService setupService,
-        //    IApplicationState applicationState,
-        //    PlatformHelper platformHelper)
-        //    : base(windowPresenter, exceptionHandler, userSettings, setupService, applicationState, platformHelper)
-        //{
-        //}
-
         public override void Start()
         {
             Application.Run((Form)MainWindow);
@@ -75,77 +62,6 @@ namespace CruiseManager.WinForms.App
             _convertedFilePath = null;
             //_convertDialog = null;
             _converter = null;
-        }
-
-        [STAThread]
-        static void Main()
-        {
-            //just trying out an alternative to the default trace listener
-            //System.Diagnostics.Trace.Listeners.Add(
-            //        new System.Diagnostics.XmlWriterTraceListener("CSMLog.xml")
-            //    );
-
-            /*side note about Trace Listeners:
-             * Tracing is a feature built into .net applications to allow
-             * applications to make logs and can be configured using the app.config
-             * file.
-             */
-
-            System.Diagnostics.Trace.TraceInformation("Application Started @{0}", DateTime.Now);
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-
-            //read command line arguments
-            var args = Environment.GetCommandLineArgs();
-            string dalPath = null;
-            if (args.Length > 1)
-            {
-                dalPath = args[1];
-            }
-
-            //Provide event handlers to catch any uncaught exceptions
-            //AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
-            //FMSC.Utility.ErrorHandling.ErrorHandlers.SendToAddress = "benjaminjcampbell@fs.fes.us";
-            //AppDomain.CurrentDomain.UnhandledException += FMSC.Utility.ErrorHandling.ErrorHandlers.UnhandledException;
-            //Application.ThreadException += FMSC.Utility.ErrorHandling.ErrorHandlers.ThreadException;
-
-            //#if !DEBUG
-            NBug.Settings.UIMode = NBug.Enums.UIMode.Full;
-            NBug.Settings.StoragePath = NBug.Enums.StoragePath.WindowsTemp;
-            NBug.Settings.Destinations.Add(new NBug.Core.Submission.Tracker.Redmine()
-            {
-                ApiKey = "6cf4343091c7509dbf27d6afd84a267189b9d3b9",
-                CustomSubject = "CrashReport",
-                Url = "http://fmsc-projects.herokuapp.com/projects/csm/",
-                ProjectId = "csm",
-                TrackerId = "5",
-                PriorityId = "1",
-                StatusId = "1"
-            });
-
-            NBug.Settings.ReleaseMode = true;//only create error reports if debugger not attached
-            NBug.Settings.StopReportingAfter = 60;
-
-            AppDomain.CurrentDomain.UnhandledException += NBug.Handler.UnhandledException;
-            Application.ThreadException += NBug.Handler.ThreadException;
-            //#endif
-
-            ApplicationControllerBase applicationController = new ApplicationController(
-                new ViewModule(),
-                new CruiseManagerWinformsModule());
-
-            if (dalPath != null)
-            {
-                applicationController.OpenFile(dalPath);
-            }
-            applicationController.Start();
-            applicationController.Dispose();
-
-            //WindowPresenter.Instance.Run();
-            //WindowPresenter.Instance.Dispose();
-
-            System.Diagnostics.Trace.TraceInformation("Application Ended @{0}", DateTime.Now);
-            System.Diagnostics.Trace.Close();
         }
     }
 }
