7d537b4d8e8f7ad6a9cefe3673ae680ae4cd5018
Ben Campbell <benjaminjcampbell@fs.fed.us>
2017-03-08 14:56:07 -0700

    only display date version in title bar

--- CruiseManager.Core/App/WindowPresenter.cs
+++ CruiseManager.Core/App/WindowPresenter.cs
@@ -104,7 +104,7 @@ namespace CruiseManager.Core.App
             this.ApplicationController.NavigateTo<IHomeView>();
 
             var version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
-            this.ApplicationController.MainWindow.Text = $"Cruise Manager {version}";
+            this.ApplicationController.MainWindow.Text = $"Cruise Manager {version.ToString(3)}";
 
             this.ApplicationController.MainWindow.SetNavCommands(new BindableCommand[]
             {
