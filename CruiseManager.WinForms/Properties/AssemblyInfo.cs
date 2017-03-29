dda7ca3d87f361b8a0dc5b4a9e4c15c0ec9f87e5
Ben Campbell <benjaminjcampbell@fs.fed.us>
2017-03-29 15:49:57 -0600

    update version ids

--- CruiseManager.WinForms/Properties/AssemblyInfo.cs
+++ CruiseManager.WinForms/Properties/AssemblyInfo.cs
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2017.03.06.*")]
+[assembly: AssemblyVersion("2017.03.29.*")]
