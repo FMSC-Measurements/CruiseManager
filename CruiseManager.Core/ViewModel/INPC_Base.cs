048c76326068181b8bc9f65b24fa6f8f2c8862a9
Ben Campbell <benjaminjcampbell@fs.fed.us>
2017-06-07 16:52:33 -0600

    add Inpc base class to presenter

---
+++ CruiseManager.Core/ViewModel/INPC_Base.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CruiseManager.Core.ViewModel
+{
+    public abstract class INPC_Base : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propName = null)
+        {
+            OnPropertyChanged(new PropertyChangedEventArgs(propName));
+        }
+
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs ea)
+        {
+            PropertyChanged?.Invoke(this, ea);
+        }
+
+        public void SetValue<T>(T value, ref T target, [CallerMemberName] string propName = null)
+        {
+            target = value;
+            OnPropertyChanged(propName);
+        }
+    }
+}
