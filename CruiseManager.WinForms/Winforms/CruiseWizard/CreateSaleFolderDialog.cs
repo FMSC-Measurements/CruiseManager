9fb316b584fee2e8b4dfdef53096546d8c4250db
rhiltonFS <rhilton@fs.fed.us>
2017-07-25 13:01:11 -0600

    Feature #727
    #727

---
+++ CruiseManager.WinForms/Winforms/CruiseWizard/CreateSaleFolderDialog.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Forms;
+
+namespace CruiseManager.WinForms.CruiseWizard
+{
+    public partial class CreateSaleFolderDialog : Form
+    {
+        public CreateSaleFolderDialog()
+        {
+            InitializeComponent();
+        }
+
+        public bool RememberSelection
+        {
+            get
+            {
+                return this.checkBox1.Checked;
+            }
+        }
+
+
+    }
+}
