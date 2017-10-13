1d518ee1a898bc8607cef59f4cb527d88a0bd544
Ben Campbell <benjaminjcampbell@fs.fed.us>
2017-10-13 15:46:44 -0400

    update version and build scripts

--- Build_Release.cmd
+++ Build_Release.cmd
@@ -10,9 +10,14 @@ SET parent=%~dp0
 ECHO %me%
 ::::::::::::::
 
-SET msbuildPath="C:\Program Files (x86)\MSBuild\14.0\bin\amd64\MSBuild.exe"
+REM use VS 2017 MSBuild 
+SET msbuildPath="C:\Program Files (x86)\MSBuild\15.0\bin\amd64\MSBuild.exe"
 
-%msbuildPath%  %parent%\CruiseManager.WinForms\CruiseManager.WinForms.VS15.csproj /target:Rebuild /property:Configuration=Release;Platform=AnyCPU;SolutionDir=%parent%\
+ECHO "falling back on VS 2015"
+IF NOT EXIST %msbuildPath% (
+	SET msbuildPath="C:\Program Files (x86)\MSBuild\14.0\bin\amd64\MSBuild.exe")
+
+%msbuildPath%  %parent%\CruiseManager.WinForms\CruiseManager.WinForms.csproj /target:Rebuild /property:Configuration=Release;Platform=AnyCPU;SolutionDir=%parent%\
 
 ::End Boilderplate
 EXIT /B %errorlevel%
