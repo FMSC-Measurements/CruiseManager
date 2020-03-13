4a054d371b9ce687fc28691ed21e03fe5261e308
Ben Campbell <benjocamps@gmail.com>
2020-03-13 10:51:27 -0400

    fix edit templates save
    fix crash when inserting duplicate items by setting conflict resolution to overwrite,
    fix editor saying changes exist when no changes made

---
+++ CruiseManager.Core/Util/IEnumerableExtentions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CruiseManager.Core.Util
+{
+    public static class IEnumerableExtentions
+    {
+        public static IEnumerable<T> OrEmpty<T>(this IEnumerable<T> @this)
+        {
+            return @this ?? Enumerable.Empty<T>();
+        }
+
+        public static IEnumerable OrEmpty(this IEnumerable @this)
+        {
+            return @this ?? Enumerable.Empty<object>();
+        }
+
+        public static bool AnyAndNotNull<T>(this IEnumerable<T> @this)
+        {
+            return @this != null && @this.Any();
+        }
+
+        public static bool AnyAndNotNull<T>(this IEnumerable<T> @this, Func<T, bool> predicate)
+        {
+            return @this != null && @this.Any(predicate);
+        }
+
+        public static int MaxOrDefault<T>(this IEnumerable<T> @this, Func<T, int> selector, int dVal = default(int))
+        {
+            if (@this.AnyAndNotNull())
+            { return @this.Max(selector); }
+            else
+            { return dVal; }
+        }
+
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> e)
+        {
+            return e == null || e.Any() == false;
+        }
+    }
+}
